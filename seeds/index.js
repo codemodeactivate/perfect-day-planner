e7b77dafda94745cdd47de09dfa730bf4c49f222
codemodeactivate <jeff.zenko@gmail.com>
2023-05-25 19:12:40 -0400

    seedOptionSets.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,22 @@
 //seedsIndex
+const seedOptionSets = require('./seedOptionSets');
+const seedPerfectDay = require('./seedPerfectDay');
+const seedSelectedOptions = require('./seedSelectedOptions');
+const seedUsers = require('./seedUsers');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+
+    await seedUsers();
+    await seedOptionSets();
+    await seedPerfectDay();
+    await seedSelectedOptions();
+    await seedUsers();
+
+    process.exit(0);
+
+};
+
+seedAll();
--- seeds/seedOptionSets.js
+++ seeds/seedOptionSets.js
@@ -1,1 +1,18 @@
 //optionsetsseeds
+const OptionSets = require('../models/OptionSets');
+const faker = require('faker');
+
+const seedOptionSets = async (numCategories = 10) => {
+    const optionSetsData = [];
+
+    for (let i = 0; i < numCategories; i++) {
+        const optionSets = {
+            category: faker.lorem.words(Math.floor(Math.random() * 3) + 1),
+            perfect_day_id: Math.floor(Math.random() * 10) + 1
+        };
+        optionSetsData.push(optionSets);
+    }
+    await OptionSets.bulkCreate(optionSetsData);
+};
+
+module.exports = seedOptionSets;
