a02328f375dd0ac6290d8c2eef1de18256b256b2
codemodeactivate <jeff.zenko@gmail.com>
2023-05-24 21:41:14 -0400

    models and associations

---
+++ models/OptionSets.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class OptionSet extends Model {}
+
+OptionSet.init(
+    {
+        option1: DataTypes.STRING,
+        option2: DataTypes.STRING,
+        perfect_day_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'perfect_day',
+                key: 'id'
+        }
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'option_set'
+});
+
+module.exports = OptionSet;
