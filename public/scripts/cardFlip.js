d8f1aabaaf9a0c24f003cb3e6b347c81e8fb164d
cschmidt216 <iamdrainy@gmail.com>
2023-05-25 21:08:13 -0700

    placeholder flipping card on landing

---
+++ public/scripts/cardFlip.js
@@ -0,0 +1,19 @@
+var flipCard = document.querySelector(".flipCard");
+var playing = false;
+
+flipCard.addEventListener("click", () => {
+  if (playing) return;
+
+  playing = true;
+
+  anime({
+    targets: flipCard,
+    scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
+    rotateY: { value: "+=180", delay: 200 },
+    easing: "easeInOutSine",
+    duration: 400,
+    complete: function (anim) {
+      playing = false;
+    }
+  });
+});
