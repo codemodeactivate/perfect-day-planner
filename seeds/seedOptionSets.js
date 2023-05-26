ee5d20c3f36382c4d6ee653fa8917cb875f0c5f4
codemodeactivate <jeff.zenko@gmail.com>
2023-05-25 20:00:09 -0400

    all tables populated, not relating

--- seeds/seedOptionSets.js
+++ seeds/seedOptionSets.js
@@ -1,18 +1,17 @@
-//optionsetsseeds
-const OptionSets = require('../models/OptionSets');
+const OptionSet = require('../models/OptionSets');
+const PerfectDay = require('../models/PerfectDays');
 const faker = require('faker');
 
-const seedOptionSets = async (numCategories = 10) => {
-    const optionSetsData = [];
+const seedOptionSets = async (numSets = 10) => {
+  const perfectDay = await PerfectDay.findOne(); // Assuming there's at least one PerfectDay record in the database
 
-    for (let i = 0; i < numCategories; i++) {
-        const optionSets = {
-            option1: faker.lorem.words(Math.round(Math.random() * 5) + 1),
-            option2: faker.lorem.words(Math.round(Math.random() * 5) + 1),
-        };
-        optionSetsData.push(optionSets);
-    }
-    await OptionSets.bulkCreate(optionSetsData);
+  for (let i = 0; i < numSets; i++) {
+    const optionSet = await OptionSet.create({
+      option1: faker.lorem.words(Math.round(Math.random() * 5) + 1),
+      option2: faker.lorem.words(Math.round(Math.random() * 5) + 1),
+      perfect_day_id: perfectDay.id
+    });
+  }
 };
 
 module.exports = seedOptionSets;
