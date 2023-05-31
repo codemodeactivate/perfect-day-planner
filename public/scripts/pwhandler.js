d024b3046f4de5b1efb31ff8052bdf05429056d6
codemodeactivate <jeff.zenko@gmail.com>
2023-05-31 13:27:24 -0400

    more enhancements, OK for now. will need to work on client side once we know CSS framework

--- public/scripts/pwhandler.js
+++ public/scripts/pwhandler.js
@@ -1,5 +1,6 @@
 
 document.querySelector(".signup-form").addEventListener("submit", function(event) {
+    event.preventDefault();
     const password = document.querySelector('input[name="password"]').value;
     const confirmPassword = document.querySelector('input[name="confirmPassword"]').value;
 
@@ -16,5 +17,28 @@ document.querySelector(".signup-form").addEventListener("submit", function(event
         event.preventDefault();
         alert("Password is too weak.");
     }
-});
 
+    // Send the form data to the server
+    fetch('/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: document.querySelector('input[name="username"]').value,
+            email: document.querySelector('input[name="email"]').value,
+            password: password,
+            confirmPassword: confirmPassword
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.error) {
+            document.getElementById('error-message').textContent = data.error;
+        } else {
+            window.location.href = "/dashboard";
+        }
+    })
+    .catch(error => console.error('Error:', error));
+
+});
