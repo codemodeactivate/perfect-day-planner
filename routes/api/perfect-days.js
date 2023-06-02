6885d19e4d51001cbe6b3f3aa4a1f9cb44dfb99e
thejamesdouglas <125297038+thejamesdouglas@users.noreply.github.com>
2023-06-01 21:25:18 -0400

    Commit stages changes

--- routes/api/perfect-days.js
+++ routes/api/perfect-days.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const controllers = require('../../controllers');
-//const withAuth = require('../middleware/auth');
 
 // Define controllers
-
-
 router.post('/', controllers.perfectDayController.create);
 
 module.exports = router;
