115ab00865a3eb1850c13215c2d94e85fbb65e11
codemodeactivate <jeff.zenko@gmail.com>
2023-06-04 12:43:07 -0400

    fix typo

--- routes/perfectdays.js
+++ routes/perfectdays.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers');
 
-router.get('/:id/view', controllers.perfectDayController.guesView)
+router.get('/:id/view', controllers.perfectDayController.guestView)
 router.put('/:id', controllers.perfectDayController.edit);
 router.get('/:id/edit', controllers.viewsController.renderPerfectDayEdit);
 
