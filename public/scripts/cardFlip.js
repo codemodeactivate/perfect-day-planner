bba08438744884e37b49b8d5170b3379bb09831f
cschmidt216 <iamdrainy@gmail.com>
2023-06-05 15:28:36 -0700

    added flip cards to guest view and slight style changes

--- public/scripts/cardFlip.js
+++ public/scripts/cardFlip.js
@@ -1,19 +1,21 @@
-var flipCard = document.querySelector(".flip-card");
+var flipCards = document.querySelectorAll(".flip-card");
 var playing = false;
 
-flipCard.addEventListener("click", () => {
-  if (playing) return;
+flipCards.forEach(function(flipCard) {
+  flipCard.addEventListener("click", () => {
+    if (playing) return;
 
-  playing = true;
+    playing = true;
 
-  anime({
-    targets: flipCard,
-    scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
-    rotateY: { value: "+=180", delay: 200 },
-    easing: "easeInOutSine",
-    duration: 400,
-    complete: function (anim) {
-      playing = false;
-    }
+    anime({
+      targets: flipCard,
+      scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
+      rotateY: { value: "+=180", delay: 200 },
+      easing: "easeInOutSine",
+      duration: 400,
+      complete: function (anim) {
+        playing = false;
+      }
+    });
   });
 });
